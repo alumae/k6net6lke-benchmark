c9688e83ac7c635c50a656ee3d8d677ae95e116e
Tanel Alumäe <alumae@gmail.com>
2023-12-04 10:53:13 +0000

    Added MT results for Google Translate API

---
+++ utils/google-api-translate.py
@@ -0,0 +1,33 @@
+import sys
+import html
+from google.cloud import translate_v2 as translate
+
+def translate_file(input_file, source_language, target_language, output_file):
+    translate_client = translate.Client()
+
+    # Read the input file
+    with open(input_file, 'r', encoding='utf-8') as file:
+        text = file.read()
+
+    # Translate the text
+    translation = translate_client.translate(text, source_language=source_language, target_language=target_language)
+
+    # Decode HTML entities in the translated text
+    decoded_text = html.unescape(translation['translatedText'])
+
+    # Write the translated text to the output file
+    with open(output_file, 'w', encoding='utf-8') as file:
+        file.write(decoded_text + "\n")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: python script.py input_file source_language target_language output_file")
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+    source_language = sys.argv[2]
+    target_language = sys.argv[3]
+    output_file = sys.argv[4]
+
+    translate_file(input_file, source_language, target_language, output_file)
+
