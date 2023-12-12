82012a633d45fb0b8e3a071924785809f351e77d
Tanel Alumäe <alumae@gmail.com>
2023-12-12 09:46:19 +0000

    Added outputs for ASR+Neurotõlge

--- utils/neurotolge-translate.py
+++ utils/neurotolge-translate.py
@@ -7,33 +7,48 @@ from pathlib import Path
 
 def neurotolge_translate(src_list, src_lang, tgt_lang):
     response = requests.post("https://api.tartunlp.ai/translation/v2", json={"text": src_list, "src": src_lang, "tgt": tgt_lang})
-
     try:
-        return response.json()['result']
-    except:
-        sys.stderr.write("Error, request code: {}\n".format(response.status_code))
-        return None
+        return response.json()['result']        
+    except Exception as e:
+        raise e
+ 
+def partition(strings, max_chars=5000):
+    """
+    Partitions a list of strings into sublists, each with a total character count
+    not exceeding max_chars.
 
-def partition(lines, chunk_size=50):
-    lines_copy = lines
+    :param strings: List of strings to partition.
+    :param max_chars: Maximum number of characters allowed in each sublist.
+    """
+    current_sublist = []
+    current_count = 0
 
-    while lines_copy:
-        yield(lines_copy[:chunk_size])
-        lines_copy = lines_copy[chunk_size:]
+    for string in strings:
+        if current_count + len(string) > max_chars:
+            if current_sublist:
+                yield current_sublist
+            current_sublist = [string]
+            current_count = len(string)
+        else:
+            current_sublist.append(string)
+            current_count += len(string)
 
-def translate_file(src_lang, tgt_lang, infile, outfile, transl_func=neurotolge_translate, chunk=50):
+    if current_sublist:
+        yield current_sublist
+
+def translate_file(src_lang, tgt_lang, infile, outfile, transl_func=neurotolge_translate, max_chars=5000):
     with open(infile, 'r') as file_handle:
         lines = [line.strip() for line in file_handle]
 
     with open(outfile, 'w') as output_file_handle:
-        for chunk in partition(lines, chunk_size=chunk):
+        for chunk in partition(lines, max_chars=max_chars):
             translations = transl_func(chunk, src_lang, tgt_lang)
 
             try:
                 for translation in translations:
                     print(translation, file=output_file_handle)
             except TypeError as error:
-                sys.stderr.write("AAA: {}\n".format(len(" ".join(chunk))))
+                sys.stderr.write("Error: {}\n".format(len(" ".join(chunk))))
                 raise error
 
 def main():
